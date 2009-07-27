7be5c6aa3251e2fe73aac7fd53739b36ba9b0c9f
gentilboulet <gentilboulet@gmail.com>
2009-07-27 17:31:06 +0200

    Resolving issues with new argument passing to modules

--- lib/Giraf/Modules/LicheDice.pm
+++ lib/Giraf/Modules/LicheDice.pm
@@ -13,7 +13,7 @@ use List::Util qw(sum);
 
 sub init {
 	my ($kernel,$irc) = @_;
-	Giraf::Trigger::register('public_function','LicheDice','bot_liche_dice',\&bot_liche_dice,'(L|l)iche\s+\d{1,4}(D|d)\d{1,8}');
+	Giraf::Trigger::register('public_function','LicheDice','bot_liche_dice',\&bot_liche_dice,'[Ll]iche');
 }
 
 sub unload {
@@ -22,30 +22,32 @@ sub unload {
 
 sub bot_liche_dice {
 	my($nick, $dest, $what)=@_;
-	Giraf::Core::debug("bot_liche_dice()");
-	$what=~m/^[Ll]iche\s+(\d{1,4})[Dd](\d{1,8})$/;
+	Giraf::Core::debug("bot_liche_dice($what)");
 	my @return;
-	my $nbroll=$1;
-	my $nbface=$2;
-	my @rolls;
-	my ($roll,$message,$rollvalue,$ligne);
-	$message = $nick." : ";
-	for($roll=1;$roll<=$nbroll;$roll = $roll + 1) {
-		$rollvalue=roll_dice($nbface);
-		push(@rolls,$rollvalue);
-		if($nbroll<20) {
-			$message = $message.$rollvalue;
-			if($roll<$nbroll) {
-				$message = $message." ,";
+	if($what=~/^(\d{1,4})[Dd](\d{1,8})$/)
+	{
+		my $nbroll=$1;
+		my $nbface=$2;
+		my @rolls;
+		my ($roll,$message,$rollvalue,$ligne);
+		$message = $nick." : ";
+		for($roll=1;$roll<=$nbroll;$roll = $roll + 1) {
+			$rollvalue=roll_dice($nbface);
+			push(@rolls,$rollvalue);
+			if($nbroll<20) {
+				$message = $message.$rollvalue;
+				if($roll<$nbroll) {
+					$message = $message." ,";
+				}
 			}
 		}
+		if($nbroll>=20) {
+			$message = $message." ".$nbroll."d".$nbface;
+		}
+		$message = $message. " - Total : ".sum(@rolls);
+		$ligne={action=>"MSG",dest=>$dest,msg=>$message};
+		push(@return,$ligne);
 	}
-	if($nbroll>=20) {
-		$message = $message." ".$nbroll."d".$nbface;
-	}
-	$message = $message. " - Total : ".sum(@rolls);
-	$ligne={action=>"MSG",dest=>$dest,msg=>$message};
-	push(@return,$ligne);
 	return @return;
 }
 
@@ -59,15 +61,15 @@ sub roll_dice {
 	if(($preliminaryroll>=20) && ($preliminaryroll<40)) {
 		$res=int(rand($facenb)/16)+1;
 	}
-        if(($preliminaryroll>=40) && ($preliminaryroll<60)) {
-                $res=int(rand($facenb)/8)+1;
-        }
-        if(($preliminaryroll>=60) && ($preliminaryroll<80)) {
-                $res=int(rand($facenb)/4)+1;
-        }
-        if(($preliminaryroll>=80) && ($preliminaryroll<90)) {
-                $res=int(rand($facenb)/2)+1;
-        }
+	if(($preliminaryroll>=40) && ($preliminaryroll<60)) {
+		$res=int(rand($facenb)/8)+1;
+	}
+	if(($preliminaryroll>=60) && ($preliminaryroll<80)) {
+		$res=int(rand($facenb)/4)+1;
+	}
+	if(($preliminaryroll>=80) && ($preliminaryroll<90)) {
+		$res=int(rand($facenb)/2)+1;
+	}
 	if($preliminaryroll>=90) {
 		$res=int(rand($facenb))+1;
 	}
